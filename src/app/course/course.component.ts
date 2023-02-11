95b7f977ec2d02876c5d1b6ce2dd4064d3f5030e
Aurélien VITA <aurelien@leandco.fr>
2023-02-11 11:18:43 +0100

    exo1

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,28 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.scss']
+  styleUrls: ['./course.component.scss'],
 })
 export class CourseComponent {
-  public compteur = 0;
+  public compteur = 0
 
-  incrementCompteur(increment: number) : void {
-    this.compteur = this.compteur + increment;
+  incrementCompteur(increment: number): void {
+    this.compteur = this.compteur + increment
   }
 
-  resetCompteur() : void {
-    this.compteur = 0;
+  resetCompteur(): void {
+    this.compteur = 0
   }
 
-  getColorCompteur() : string {
-    if(this.compteur === 0)
+  getColorCompteur(): string {
+    if (this.compteur === 0)
       return 'yellow'
-    else if(this.compteur >=1) 
+    else if (this.compteur >= 1)
       return '#009c29'
     else
       return '#ab000f'
   }
-  
 }
---
+++ uno.config.js
@@ -0,0 +1,7 @@
+export default {
+  // presets
+  uno: true, // enabled `@unocss/preset-uno`
+  icons: true, // enabled `@unocss/preset-icons`
+  attributify: true, // enabled `@unocss/preset-attributify`,
+  tagify: true,
+}
