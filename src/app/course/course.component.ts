cdb7d4543b03f2f55d801c9f41c3198e781804d5
Aurélien VITA <aurelien@leandco.fr>
2023-02-11 10:15:00 +0100

    feat: exo1

---
+++ src/app/course/course.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.scss']
+})
+export class CourseComponent {
+  public compteur = 0;
+
+  incrementCompteur(increment: number) : void {
+    this.compteur = this.compteur + increment;
+  }
+
+  resetCompteur() : void {
+    this.compteur = 0;
+  }
+
+  getColorCompteur() : string {
+    if(this.compteur === 0)
+      return 'yellow'
+    else if(this.compteur >=1) 
+      return '#009c29'
+    else
+      return '#ab000f'
+  }
+  
+}
